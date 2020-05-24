79d53e023d1c26df751f812d0b028509eaa97e39
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-24 17:07:05 +0200

    Pagina para poner faltas

--- Faltas.php
+++ Faltas.php
@@ -9,9 +9,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="shortcut icon" href="img/logoAula.png" type="image/png">
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <title>Faltas</title>
 </head>
 
