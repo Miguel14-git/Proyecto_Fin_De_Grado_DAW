e93412f7fd2c234e1f244a7d5007dafb1603ecbd
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-24 17:15:48 +0200

    Página para matricular alumnos

--- Matricula.php
+++ Matricula.php
@@ -11,9 +11,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/logoAula.png" type="image/png">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
+    <link rel="stylesheet" href="css/grafica.css">
+    <script src="js/jquery-3.5.1.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Matricula Alumnos</title>
 </head>
@@ -33,10 +35,25 @@
 
 <div class="container-fluid">
 
-        <div class="row">
+<div class="row">
             <div class="col-sm-12">
-            <pre id="alumnos" class="text-center text-dark alert alert-primary"></pre>
-            <h1 id="error"></h1>
+                <fieldset class="insertar">
+                    <legend>Introduce los datos</legend>
+                    <form>
+                        <div class="form-group" draggable="true">
+                            <input type="file" class="from-control-file" id="fichero">
+                        </div>
+                    </form>
+                </fieldset>
+            </div>
+        </div>
+
+        <div class="row mt-2">
+            <div class="col-sm-12 alert alert-secondary">
+            
+                <ul id="lista_alumnos" class="list-group">
+
+                </ul>
             </div> 
         </div>
         <hr>
@@ -56,24 +73,7 @@
     </div>
 </div>
 
-    <script type="text/javascript">
-            let p = document.getElementById("alumnos");
-            let capaError = document.getElementById("error");
-
-                fetch ("Fichero.txt")
-                    .then(function (respuesta){
-                        return respuesta.text()
-                    })
-
-                    .then (function (texto){
-                        console.log(texto);
-                        p.innerHTML = texto;
-                    })
-
-                    .catch (function (error){
-                        capaError.innerHTML = error;
-                    });
-    </script>
+    <script src="js/matricula.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/popper.min.js"></script>
 </body>
