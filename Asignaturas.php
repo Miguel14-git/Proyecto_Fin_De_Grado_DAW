05fc0ae88cff1eeeec530dd91d11ad60f4ad9a4d
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-24 17:02:22 +0200

    Página para seleccionar asignaturas a impartir

--- Asignaturas.php
+++ Asignaturas.php
@@ -11,9 +11,10 @@
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,shrink-to-fit=no">
+    <link rel="shortcut icon" href="img/logoAula.png" type="image/png">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/estilo.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Seleccionar Asignaturas a Impartir</title>
 </head>
