c777a71ec6a43fb378130e3d852894c99ca8746f
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-25 18:24:49 +0200

    Página de Login

--- Login.php
+++ Login.php
@@ -39,8 +39,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="shortcut icon" href="img/logoAula.png" type="image/png">
     <link rel="stylesheet" href="css/estilo.css">
-    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
     <title>Página de Inicio de sesion</title>
 </head>
 <body>
