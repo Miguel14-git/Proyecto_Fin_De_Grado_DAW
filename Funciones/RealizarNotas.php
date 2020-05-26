3925ba5acaad14a5775a480f2339a98c4a981a87
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-26 17:11:10 +0200

    Funcion PHP para buscar el alumno a poner nota

--- Funciones/RealizarNotas.php
+++ Funciones/RealizarNotas.php
@@ -6,7 +6,9 @@
         <title>Notas</title>
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/estilo.css">
-        <script src="../js/jquery-3.4.1.min.js"></script>
+        <link rel="shortcut icon" href="../img/logoAula.png" type="image/png">
+        <script src="../js/jquery-3.5.1.min.js"></script>
+        <script src="..js/bootstrap.min.js"></script>
     </head>
 
     <body>
@@ -68,7 +70,7 @@
                 echo "<option>" .$año."</option>";
                 echo '</select>';
                 echo '</td>';
-                echo '<td><input type="number" name="nota" class="form-control">';
+                echo '<td><input type="number" name="nota" class="form-control" min=0 max=10>';
                 echo '</td>';
                 echo'</tr>';
                 print "</table>";
@@ -79,7 +81,9 @@
                 
             ?>
 
+                <a class='btn btn-success' href='../Notas.php' role='button'>Ir a poner notas</a>
             
         </div>
+        <script src="..js/popper.min.js"></script>
     </body>
 </html>
