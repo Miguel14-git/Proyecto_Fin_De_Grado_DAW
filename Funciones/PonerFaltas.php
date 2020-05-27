48ffe178e08189b8cd8b52639985272ef249e43f
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-27 12:09:10 +0200

    Funcion PHP para realizar la falta

--- Funciones/PonerFaltas.php
+++ Funciones/PonerFaltas.php
@@ -4,36 +4,19 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Realizacion de Faltas</title>
-        <link rel="shortcut icon" href="../img/logoAula.png" type="image/x-icon">
+        <link rel="shortcut icon" href="../img/logoAula.png" type="image/png">
         <link rel="stylesheet" href="../css/bootstrap.min.css">
         <link rel="stylesheet" href="../css/estilo.css">
         <script src="../js/jquery-3.5.1.min.js"></script>
         <script src="../js/bootstrap.min.js"></script>
+        
     </head>
 
     <body>
-
-    <div class="container">
-
-<!-- Button to Open the Modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-  Abrir Mensaje
-</button>
-
-<!-- The Modal -->
-<div class="modal" id="myModal">
-  <div class="modal-dialog">
-    <div class="modal-content">
     
-      <!-- Modal Header -->
-      <div class="modal-header">
-        <h4 class="modal-title">Mensaje</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-      </div>
-      
-      <!-- Modal body -->
-      <div class="modal-body">
-      <?php  
+    <div class="container-fluid">
+    
+    <?php  
             
             require_once "Conexion.php";
             comprobar_sesion();
@@ -45,42 +28,76 @@
 
             $bd = new PDO($cadena_conexion,$usuario,$clave);
 
+            echo "<ul class= 'list-group'>";
+            echo "<li class= 'list-group-item'>Id_Alumno elegido :" . $id_alumno = $_POST['id_alumno'] ;
+            echo "</li>";
+            echo "<li class= 'list-group-item'>Año Academico elegido :" . $año = $_POST['año'] ;
+            echo "</li>";
+            echo "<li class= 'list-group-item'> Id_Asignatura elegido :" . $id_asignatura = $_POST['asignatura'] ;
+            echo "</li>";
+            echo "</ul>";
 
-            $id_alumno = $_POST['id_alumno'];
-            $año = $_POST['año'];
-            $id_asignatura = $_POST['asignatura'];
-            $fecha = date("Y-m-d H:i:s", time());
+            $select = "Select Id_Asignatura from impartir where Año_Academico = '$año' and Id_Profesor = '$_SESSION[id_usuario]'";
+            $test = $bd -> query($select);
 
-           
+            foreach ($test as $s){
 
-            
-            $insert = "Insert into faltas (Id_Alumno,Id_Asignatura,Fecha_Hora,Año_Academico) values ('$id_alumno','$id_asignatura','$fecha','$año')";
-            $resultado = $bd->query($insert);
+                $id = $s['Id_Asignatura'];
+                 $año;
 
-            if($resultado === false){
-                echo '<p class="text-center display-4 mt-5">Falta no insertada</p>';
-            }else{
-                echo "<p class='text-center display-4 mt-5'>El alumno $id_alumno tiene la falta en el año $año , en la asignatura $id_asignatura con fecha de $fecha</p>";
+                $tmp[] = $id;
             }
+
+            echo "<div class='alert alert-secondary mt-3' role='alert'>Id_Asignaturas impartidas según el año academico : ". implode(" ", $tmp) . "</div>";
+            
+
+                    echo '<form action="InsertarFaltas.php" method="POST">';
+                    print '<div class="row d-flex justify-content-center">';
+                    print '<h1>Tabla del alumno a poner el retraso</h1>';
+                    print '<table style = width:65% class="table table-striped table-hover table-bordered">';
+                    echo '<thead class="thead-dark">';
+                    echo '<tr align="center">';
+                    echo "<th align = 'center'> Codigo </th>";
+                    echo "<th align = 'center'> Año_Academico </th>";
+                    echo "<th align = 'center'> Id_Asignatura </th>";
+                    echo '</tr>';
+                    echo '</thead>';
+                    echo '<tr class = "text-center">';
+
+                    echo '<td><select name="id_alumno" class="form-control">';
+                    echo '<option>'.$id_alumno.'</option>';
+                    echo '</select>';
+                    echo '</td>';
+
+                    echo '<td><select name="año_acade" class="form-control">';
+                    echo '<option>'.$año.'</option>';
+                    echo '</select>';
+                    echo '</td>';
+
+                    echo '<td><select name="id_asignatura" class="form-control">';
+                        foreach ($tmp as $s){
+                        
+                            $consultar = "Select Nombre_Asignatura from asignaturas where Id_Asignatura = $s";
+                            $r = $bd -> query($consultar);
+
+                            foreach ($r as $as){
+                                echo '<option>' .$s." " .$as['Nombre_Asignatura'].'</option>';
+                            }
+                        }               
+                        echo '</select>';
+                        echo '</td>';                 
+
+                    print "</tr>";
+                    print "</table>";
+                    print "</div>";
+                    print '<input type="submit" name="enviar" value="Enviar Faltas" class="mt-4 mb-4 ml-4 w-25 text-center btn btn-warning">';
+                    echo '</form>';
 
 
         ?>
-      </div>
-      
-      <!-- Modal footer -->
-      <div class="modal-footer">
-        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-success"><a class='btn btn-success' href='../Faltas.php' role='button'>Ir a poner faltas</a></button>
-      </div>
-      
+        <a class='btn btn-success' href='../Faltas.php' role='button'>Ir a poner faltas</a>
     </div>
-  </div>
-</div>
 
-</div>
-  
-</div>
-
-  <script src="../js/popper.min.js"></script>
-</body>
+    <script src="../js/popper.min.js"></script>
+    </body>
 </html>
