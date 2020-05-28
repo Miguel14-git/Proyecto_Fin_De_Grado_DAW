18d994412d81ae9877aa364643ca4a0876ec1b25
Miguel Navarro Perez <porterazopinoso1@gmail.com>
2020-05-28 17:08:30 +0200

    Página para buscar alumnos matriculados

---
+++ AlumnosMatr.php
@@ -0,0 +1,80 @@
+<?php
+
+    require_once "Funciones/Conexion.php";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="shortcut icon" href="img/logoAula.png" type="image/png">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <title>Busqueda Alumnos Matriculados</title>
+</head>
+<body>
+
+<?php
+    require "Pagina_Principal.php";
+?>
+       <hr>
+    <h1 class="text-center">Selecciona para ver el listado de alumnos matriculados en los cursos del usuario  <?php echo $_SESSION["usuario"]?></h1>
+
+    <?php
+        $cadena_conexion = 'mysql:dbname=aulavirtual_instituto;host=localhost';
+        $usuario = 'root';
+        $clave ='';
+
+        $bd = new PDO($cadena_conexion,$usuario,$clave);
+
+        $profesores = "Select Id_Asignatura from impartir where Id_Profesor = $_SESSION[id_usuario]";
+        $resultado = $bd -> query($profesores);
+
+        $profesores2 = "Select Año_Academico from impartir where Id_Profesor = $_SESSION[id_usuario]";
+        $resultado2 = $bd -> query($profesores2);
+
+
+        echo '<form action="Funciones/BusquedaAluMa.php" method="POST">';
+        print '<div class="row d-flex justify-content-center">';
+        print '<table style = width:65% class="table table-striped table-hover table-bordered">';
+        echo '<thead class="thead-dark">';
+        echo '<tr align="center">';
+        echo "<th align = 'center'> Id_Asignatura </th>";
+        echo "<th align = 'center'> Año_Academico </th>";
+        echo '</tr>';
+        echo '</thead>'; 
+        echo '<tr class = "text-center">';
+        echo '<td><select name="id_asignatura" class="form-control">';
+        foreach ($resultado as $p){
+            echo "<option>" .$id = $p['Id_Asignatura'] ." ";
+            $asignatura = "SELECT Nombre_Asignatura FROM asignaturas WHERE Id_Asignatura = $id";
+            $resul = $bd->query($asignatura);
+            foreach($resul as $nombre){
+                echo $nombre['Nombre_Asignatura'];
+            }
+            echo "</option>";
+        }
+        echo '</select>';
+        echo '</td>';
+
+        echo '<td><select name="año" class="form-control">';
+        foreach ($resultado2 as $p2){
+            echo "<option>" .$p2['Año_Academico']."</option>";
+        }
+        echo '</select>';
+        echo '</td>';
+
+        print "</tr>";
+        print "</table>";
+        echo '</div>';
+        print '<input type="submit" name="enviar" value="Enviar" class="mt-4 mb-4 ml-4 w-25 text-center btn btn-warning">';
+        echo '</form>';
+       
+    ?>
+
+    <script src="js/popper.min.js"></script>
+</body>
+</html>
